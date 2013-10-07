2b48dff29c5b6a00bb19fbadd868d9302821826f
Patrik Ingmarsson <ipatrik@student.chalmers.se>
2013-10-07 12:01:06 +0200

    Search + misc
    Börjat på Search men har också kollat lite på "WEB-INF"-filerna.

---
+++ BookExchange_Skel/src/main/java/group20/jsf/bb/serachBB.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package group20.jsf.bb;
+
+
+/**
+ *
+ * @author Patrik
+ */
+
+public class serachBB {
+
+    /**
+     * Creates a new instance of serachBB
+     */
+    public serachBB() {
+    }
+    
+    public void execute(){
+    
+    }
+}
