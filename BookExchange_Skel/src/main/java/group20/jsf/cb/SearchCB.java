e0247597a69e4c1eb0acbee17e115f680c3ad226
Patrik Ingmarsson <ipatrik@student.chalmers.se>
2013-10-09 18:49:55 +0200

    Börjar med ControlBeans

---
+++ BookExchange_Skel/src/main/java/group20/jsf/cb/SearchCB.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package group20.jsf.cb;
+
+/**
+ *
+ * @author Patrik
+ */
+public class SearchCB {
+    
+}
