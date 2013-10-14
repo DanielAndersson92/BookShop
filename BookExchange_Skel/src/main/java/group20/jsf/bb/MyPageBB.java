60eda91dc3489fc514d2e638c4236af4567ba7b2
lazica <alexandralazic@alexandra.local>
2013-10-14 11:50:59 +0200

    test

--- BookExchange_Skel/src/main/java/group20/jsf/bb/MyPageBB.java
+++ BookExchange_Skel/src/main/java/group20/jsf/bb/MyPageBB.java
@@ -8,10 +8,10 @@ import group20.bookexchange.core.Book;
 import group20.jsf.mb.ExchangeBean;
 import group20.jsf.utils.ContainerNavigator;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
-import javax.faces.model.SelectItem;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -27,12 +27,36 @@ public class MyPageBB implements Serializable {
     private ExchangeBean bookExchange;
     private Book.BookState bookState;
     private ContainerNavigator cn;
-    private List<String> selectedItems;
-    private List<SelectItem> selectItems;
+    private List<Book> wanted = new ArrayList<Book>();
+    private List<Book> forSale = new ArrayList<Book>();
+    private int numberOfBooks = 1;
     
     @PostConstruct
     public void post() {
-        initialize();
+        setForSale();
+        setWanted();
+    }
+    
+    public List<Book> getWanted(){
+    //ska endast hämta inloggade användarens böcker        
+        return wanted;
+    }
+    
+    public void setWanted(){
+        List<Book> books;
+        books = bookExchange.getBookList().getByState(Book.BookState.WANTED);
+        wanted = books.subList(0, numberOfBooks);
+    }
+    
+    public List<Book> getForSale(){
+    //ska endast hämta inloggade användarens böcker
+        return forSale;
+    }
+    
+    public void setForSale(){
+        List<Book> books;
+        books = bookExchange.getBookList().getByState(Book.BookState.FORSALE);
+        forSale = books.subList(0, numberOfBooks);
     }
     
     public List<Book> getRange() {
@@ -44,5 +68,4 @@ public class MyPageBB implements Serializable {
         cn = new ContainerNavigator(0, 10, bookExchange.getBookList());
         bookState = null;
     }
-
 }
