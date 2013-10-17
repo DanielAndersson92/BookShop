341d66e01c8e9b92c97631d75c4a0623c4b80f6d
Alexandra Lazic <alexandramlazic@gmail.com>
2013-10-17 10:16:03 +0200

    MyPage med användarens böcker

--- BookExchange_Skel/src/main/java/group20/jsf/cb/LoginBean.java
+++ BookExchange_Skel/src/main/java/group20/jsf/cb/LoginBean.java
@@ -4,11 +4,9 @@ import group20.bookexchange.core.User;
 import group20.jsf.mb.ExchangeBean;
 import java.io.Serializable;
 import java.util.logging.Logger;
+import javax.enterprise.context.SessionScoped;
  
 import javax.faces.application.FacesMessage;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -20,7 +18,7 @@ import javax.inject.Named;
  * @author itcuties
  */
 
-@ManagedBean
+@Named("loginBean")
 @SessionScoped
 public class LoginBean implements Serializable {
  
