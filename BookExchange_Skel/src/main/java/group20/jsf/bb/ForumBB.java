5619878b9ccd719fefdc7e0c0e2a82329d57be73
DanielAndersson92 <andersson.d.92@gmail.com>
2013-10-19 16:28:08 +0200

    Gästforum "funkar"

--- BookExchange_Skel/src/main/java/group20/jsf/bb/ForumBB.java
+++ BookExchange_Skel/src/main/java/group20/jsf/bb/ForumBB.java
@@ -4,12 +4,16 @@
  */
 package group20.jsf.bb;
 
+import group20.bookexchange.core.User;
 import group20.bookexchange.forum.Post;
+import group20.jsf.cb.LoginBean;
 import group20.jsf.mb.ExchangeBean;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.*;
 
@@ -19,24 +23,58 @@ import javax.inject.*;
  */
 
 @Named("forum")
-@SessionScoped
+@RequestScoped
 public class ForumBB implements Serializable {
     
     @Inject
     private ExchangeBean bookExchange;
+    @Inject
+    private LoginBean logbean;
     
     private List<Post> posts;
     
+    private String message;
+    private User author;
+    private Date date;
+    
     @PostConstruct
     public void post() {
-        setPosts();
+        posts = bookExchange.getPostList().getPosts();
     }
 
-    private void setPosts() {
+    public void sendPost(){
+        Post p = new Post(message, new Date(), logbean.getUser());
+        bookExchange.getPostList().add(p);
+    }
+    
+    
+    private void setPostar() {
         posts = bookExchange.getPostList().getPosts();
     }
     
-    private List<Post> getPosts(){
-        return posts;
+    private List<Post> getPostar(){
+        return bookExchange.getPostList().getPosts();
+    }
+    
+    private void setMessage(String message){
+        this.message = message;
+    }
+    
+    private String getMessage(){
+        return message;
+    }
+     private void setAuthor(User author){
+        this.author = author;
+    }
+    
+    private User getAuthor(){
+        return author;
+    }
+     private void setDate(Date date){
+        this.date = date;
+    }
+    
+    private Date getDate(){
+        return date;
     }
 }
