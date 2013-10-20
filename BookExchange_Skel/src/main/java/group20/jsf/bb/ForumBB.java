577e00e9270e0e27667dec42cd38fd49bd5f37cc
DanielAndersson92 <andersson.d.92@gmail.com>
2013-10-20 13:36:43 +0200

    Booktable fix

--- BookExchange_Skel/src/main/java/group20/jsf/bb/ForumBB.java
+++ BookExchange_Skel/src/main/java/group20/jsf/bb/ForumBB.java
@@ -8,10 +8,6 @@ import group20.bookexchange.forum.Post;
 import group20.jsf.cb.LoginBean;
 import group20.jsf.mb.ExchangeBean;
 import java.io.Serializable;
-<<<<<<< HEAD
-import java.util.ArrayList;
-=======
->>>>>>> e4f9690c91024c3254151ff96b28802d3b1175fb
 import java.util.Date;
 import java.util.List;
 import javax.annotation.PostConstruct;
